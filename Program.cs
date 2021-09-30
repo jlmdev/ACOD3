93397e0b34003f40f2f905f8124a81a382eab521
Josh Mann <65541818+jlmdev@users.noreply.github.com>
2021-09-30 14:08:39 -0400

    refactored printing into single function

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ACOD3
 {
   class Program
   {
+    static public void printDeck(List<string> deck) 
+    {
+      foreach (var card in deck)
+        {
+          System.Console.WriteLine(card);
+        }
+    }
+
     static void Main(string[] args)
     {
+      // Initialize variables
       var deck = new List<string>();
       var suits = new string[] {"♠️", "♥️", "♦️", "♣️"};
       var ranks = new string[] {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
@@ -19,10 +29,22 @@ namespace ACOD3
         }
       }
 
-      foreach (var card in deck)
+      printDeck(deck);
+
+      // Shuffle Deck
+      var deckCount = deck.Count();
+      var randomNumber = new Random();
+      for (var rightIndex = deckCount - 1; rightIndex > 0; rightIndex--)
       {
-        System.Console.WriteLine(card);
+        var leftIndex = randomNumber.Next(rightIndex - 1);
+        var leftCard = deck[rightIndex];
+        var rightCard = deck[leftIndex];
+
+        deck[rightIndex] = rightCard;
+        deck[leftIndex]= leftCard;
       }
+
+      printDeck(deck);
     }
   }
 }
